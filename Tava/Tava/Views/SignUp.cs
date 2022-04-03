731b0e61711cad19d126b2d25a1a985ea4b00769
Fernando Montano <fernando.montano41@gmail.com>
2022-04-03 00:39:40 -0600

    Reduced Sign Up nesting. 😵‍💫

--- Tava/Tava/Views/SignUp.cs
+++ Tava/Tava/Views/SignUp.cs
@@ -36,53 +36,57 @@ namespace Tava.Views
         {
             if (txtUsername.Text == "" || txtName.Text == "" || txtId.Text == "" ||
                 txtLastname.Text == "" || txtLastname.Text == "" || txtPassword.Text == "" || txtEmail.Text == "")
-                MessageBox.Show("Faltan campos por completar");
-            else if (WeakPassword())
-                MessageBox.Show("La contraseña debe tener más de 8 carácteres");
-            else
             {
-                try
-                {
-                    using (var database = new TavaContext())
-                    {
-                        //primero se guardan los registros en la tabla user
-                        var userData = new User();
-                        userData.Username = txtUsername.Text;
-                        userData.Password = txtPassword.Text;
-                        database.Users.Add(userData);
-                        database.SaveChanges();
-                        Id = userData.Id;
-                    }
-
-                    using (var database = new TavaContext())
-                    {
-                        //se guardan los registros en la tabla clients
-                        var adminData = new Admin
-                        {
-                            UserId = Id,
-                            Name = txtName.Text,
-                            Bankaccount = txtBank.Text,
-                            Companyname = "Tava",
-                            Identitynumber = txtId.Text,
-                            Lastname = txtLastname.Text,
-                        };
-                        //relaciona el id de la tabla user con la foreign key de la tabla client
-                        adminData.UserId = Id;
-                        database.Admins.Add(adminData);
-                        database.SaveChanges();
-                    }
-
-                    MessageBox.Show("Bienvenido a Tava SV!", "Alerta", MessageBoxButtons.OK, MessageBoxIcon.Information);
-                    var goTologin = new Login();
-                    goTologin.Show();
-                    Hide();
-                }
-                catch
-                {
-                    MessageBox.Show("El nombre de usuario, DUI o correo electrónico ya ha sido registrado");
-                }
-
+                MessageBox.Show("Faltan campos por completar");
+                return;
             }
+
+            if (WeakPassword())
+            {
+                MessageBox.Show("La contraseña debe tener más de 8 carácteres");
+                return;
+            }
+            try
+            {
+                using (var database = new TavaContext())
+                {
+                    //primero se guardan los registros en la tabla user
+                    var userData = new User();
+                    userData.Username = txtUsername.Text;
+                    userData.Password = txtPassword.Text;
+                    database.Users.Add(userData);
+                    database.SaveChanges();
+                    Id = userData.Id;
+                }
+
+                using (var database = new TavaContext())
+                {
+                    //se guardan los registros en la tabla clients
+                    var adminData = new Admin
+                    {
+                        UserId = Id,
+                        Name = txtName.Text,
+                        Bankaccount = txtBank.Text,
+                        Companyname = "Tava",
+                        Identitynumber = txtId.Text,
+                        Lastname = txtLastname.Text,
+                    };
+                    //relaciona el id de la tabla user con la foreign key de la tabla client
+                    adminData.UserId = Id;
+                    database.Admins.Add(adminData);
+                    database.SaveChanges();
+                }
+
+                MessageBox.Show("Bienvenido a Tava SV!", "Alerta", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                var goTologin = new Login();
+                goTologin.Show();
+                Hide();
+            }
+            catch
+            {
+                MessageBox.Show("El nombre de usuario, DUI o correo electrónico ya ha sido registrado");
+            }
+
         }
     }
 }
