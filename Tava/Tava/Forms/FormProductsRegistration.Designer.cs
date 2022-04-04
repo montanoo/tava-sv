18ba7b42574db91e4545b55e987a2ab0095ae996
Luciana <lucianamunguia@gmail.com>
2022-04-03 22:04:43 -0600

    Added product registration form🖼️

--- Tava/Tava/Forms/FormProductsRegistration.Designer.cs
+++ Tava/Tava/Forms/FormProductsRegistration.Designer.cs
@@ -30,10 +30,10 @@ namespace Tava.Forms
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormProductsRegistration));
-            this.textBox4 = new System.Windows.Forms.TextBox();
-            this.textBox3 = new System.Windows.Forms.TextBox();
-            this.textBox2 = new System.Windows.Forms.TextBox();
-            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.PrecioTxt = new System.Windows.Forms.TextBox();
+            this.EmpaqueTxt = new System.Windows.Forms.TextBox();
+            this.DescripcionTxt = new System.Windows.Forms.TextBox();
+            this.NombreTxt = new System.Windows.Forms.TextBox();
             this.button1 = new System.Windows.Forms.Button();
             this.label6 = new System.Windows.Forms.Label();
             this.label5 = new System.Windows.Forms.Label();
@@ -41,64 +41,74 @@ namespace Tava.Forms
             this.label3 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.label1 = new System.Windows.Forms.Label();
-            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.UnidadesTxt = new System.Windows.Forms.NumericUpDown();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
-            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.UnidadesTxt)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
-            // textBox4
+            // PrecioTxt
             // 
-            this.textBox4.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.textBox4.Location = new System.Drawing.Point(750, 203);
-            this.textBox4.Name = "textBox4";
-            this.textBox4.Size = new System.Drawing.Size(96, 30);
-            this.textBox4.TabIndex = 22;
+            this.PrecioTxt.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.PrecioTxt.Location = new System.Drawing.Point(938, 254);
+            this.PrecioTxt.Margin = new System.Windows.Forms.Padding(4);
+            this.PrecioTxt.Name = "PrecioTxt";
+            this.PrecioTxt.Size = new System.Drawing.Size(119, 34);
+            this.PrecioTxt.TabIndex = 22;
+            this.PrecioTxt.Text = "0.00";
+            this.PrecioTxt.Click += new System.EventHandler(this.PrecioTxt_Click);
             // 
-            // textBox3
+            // EmpaqueTxt
             // 
-            this.textBox3.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.textBox3.ForeColor = System.Drawing.SystemColors.ActiveBorder;
-            this.textBox3.Location = new System.Drawing.Point(604, 311);
-            this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(268, 30);
-            this.textBox3.TabIndex = 21;
-            this.textBox3.Text = "Caja, bolsa, otros.";
+            this.EmpaqueTxt.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.EmpaqueTxt.ForeColor = System.Drawing.SystemColors.ActiveBorder;
+            this.EmpaqueTxt.Location = new System.Drawing.Point(755, 389);
+            this.EmpaqueTxt.Margin = new System.Windows.Forms.Padding(4);
+            this.EmpaqueTxt.Name = "EmpaqueTxt";
+            this.EmpaqueTxt.Size = new System.Drawing.Size(334, 34);
+            this.EmpaqueTxt.TabIndex = 21;
+            this.EmpaqueTxt.Text = "Caja, bolsa, otros";
+            this.EmpaqueTxt.Click += new System.EventHandler(this.EmpaqueTxt_Click);
             // 
-            // textBox2
+            // DescripcionTxt
             // 
-            this.textBox2.Font = new System.Drawing.Font("Segoe UI", 35F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.textBox2.Location = new System.Drawing.Point(64, 267);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(411, 85);
-            this.textBox2.TabIndex = 20;
+            this.DescripcionTxt.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.DescripcionTxt.Location = new System.Drawing.Point(80, 334);
+            this.DescripcionTxt.Margin = new System.Windows.Forms.Padding(4);
+            this.DescripcionTxt.Multiline = true;
+            this.DescripcionTxt.Name = "DescripcionTxt";
+            this.DescripcionTxt.Size = new System.Drawing.Size(513, 157);
+            this.DescripcionTxt.TabIndex = 20;
             // 
-            // textBox1
+            // NombreTxt
             // 
-            this.textBox1.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.textBox1.ForeColor = System.Drawing.SystemColors.ActiveBorder;
-            this.textBox1.Location = new System.Drawing.Point(64, 170);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(411, 30);
-            this.textBox1.TabIndex = 19;
-            this.textBox1.Text = "Nombre";
+            this.NombreTxt.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.NombreTxt.ForeColor = System.Drawing.SystemColors.ActiveBorder;
+            this.NombreTxt.Location = new System.Drawing.Point(80, 212);
+            this.NombreTxt.Margin = new System.Windows.Forms.Padding(4);
+            this.NombreTxt.Name = "NombreTxt";
+            this.NombreTxt.Size = new System.Drawing.Size(513, 34);
+            this.NombreTxt.TabIndex = 19;
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(399, 412);
+            this.button1.Location = new System.Drawing.Point(499, 515);
+            this.button1.Margin = new System.Windows.Forms.Padding(4);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(211, 58);
+            this.button1.Size = new System.Drawing.Size(264, 72);
             this.button1.TabIndex = 18;
             this.button1.Text = "Registrar producto";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // label6
             // 
             this.label6.AutoSize = true;
             this.label6.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.label6.Location = new System.Drawing.Point(604, 270);
+            this.label6.Location = new System.Drawing.Point(755, 338);
+            this.label6.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(148, 23);
+            this.label6.Size = new System.Drawing.Size(169, 28);
             this.label6.TabIndex = 17;
             this.label6.Text = "Tipo de empaque:";
             // 
@@ -106,9 +116,10 @@ namespace Tava.Forms
             // 
             this.label5.AutoSize = true;
             this.label5.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.label5.Location = new System.Drawing.Point(604, 210);
+            this.label5.Location = new System.Drawing.Point(755, 262);
+            this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(57, 23);
+            this.label5.Size = new System.Drawing.Size(66, 28);
             this.label5.TabIndex = 16;
             this.label5.Text = "Precio";
             // 
@@ -116,9 +127,10 @@ namespace Tava.Forms
             // 
             this.label4.AutoSize = true;
             this.label4.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.label4.Location = new System.Drawing.Point(604, 126);
+            this.label4.Location = new System.Drawing.Point(755, 158);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(81, 46);
+            this.label4.Size = new System.Drawing.Size(94, 56);
             this.label4.TabIndex = 15;
             this.label4.Text = "Unidades\r\npor set:";
             // 
@@ -126,9 +138,10 @@ namespace Tava.Forms
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.label3.Location = new System.Drawing.Point(64, 226);
+            this.label3.Location = new System.Drawing.Point(80, 282);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(102, 23);
+            this.label3.Size = new System.Drawing.Size(118, 28);
             this.label3.TabIndex = 14;
             this.label3.Text = "Descripción:";
             // 
@@ -136,9 +149,10 @@ namespace Tava.Forms
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.label2.Location = new System.Drawing.Point(64, 126);
+            this.label2.Location = new System.Drawing.Point(80, 158);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(180, 23);
+            this.label2.Size = new System.Drawing.Size(208, 28);
             this.label2.TabIndex = 13;
             this.label2.Text = "Nombre del producto:";
             // 
@@ -146,42 +160,55 @@ namespace Tava.Forms
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Segoe UI", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.label1.Location = new System.Drawing.Point(344, 31);
+            this.label1.Location = new System.Drawing.Point(430, 39);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(278, 37);
+            this.label1.Size = new System.Drawing.Size(336, 45);
             this.label1.TabIndex = 12;
             this.label1.Text = "Registro de productos";
             // 
-            // numericUpDown1
+            // UnidadesTxt
             // 
-            this.numericUpDown1.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.numericUpDown1.Location = new System.Drawing.Point(750, 142);
-            this.numericUpDown1.Name = "numericUpDown1";
-            this.numericUpDown1.Size = new System.Drawing.Size(96, 30);
-            this.numericUpDown1.TabIndex = 23;
+            this.UnidadesTxt.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.UnidadesTxt.Location = new System.Drawing.Point(938, 178);
+            this.UnidadesTxt.Margin = new System.Windows.Forms.Padding(4);
+            this.UnidadesTxt.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.UnidadesTxt.Name = "UnidadesTxt";
+            this.UnidadesTxt.Size = new System.Drawing.Size(120, 34);
+            this.UnidadesTxt.TabIndex = 23;
+            this.UnidadesTxt.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
             // 
             // pictureBox1
             // 
             this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
-            this.pictureBox1.Location = new System.Drawing.Point(64, 31);
+            this.pictureBox1.Location = new System.Drawing.Point(80, 39);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(4);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(94, 72);
+            this.pictureBox1.Size = new System.Drawing.Size(118, 90);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.pictureBox1.TabIndex = 24;
             this.pictureBox1.TabStop = false;
             // 
             // FormProductsRegistration
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.White;
-            this.ClientSize = new System.Drawing.Size(985, 534);
+            this.ClientSize = new System.Drawing.Size(1231, 668);
             this.Controls.Add(this.pictureBox1);
-            this.Controls.Add(this.numericUpDown1);
-            this.Controls.Add(this.textBox4);
-            this.Controls.Add(this.textBox3);
-            this.Controls.Add(this.textBox2);
-            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.UnidadesTxt);
+            this.Controls.Add(this.PrecioTxt);
+            this.Controls.Add(this.EmpaqueTxt);
+            this.Controls.Add(this.DescripcionTxt);
+            this.Controls.Add(this.NombreTxt);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.label6);
             this.Controls.Add(this.label5);
@@ -189,10 +216,11 @@ namespace Tava.Forms
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
+            this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "FormProductsRegistration";
             this.Text = "FormProductsRegistration";
             this.Load += new System.EventHandler(this.FormProductsRegistration_Load);
-            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.UnidadesTxt)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
@@ -200,10 +228,10 @@ namespace Tava.Forms
         }
 
         #endregion
-        private System.Windows.Forms.TextBox textBox4;
-        private System.Windows.Forms.TextBox textBox3;
-        private System.Windows.Forms.TextBox textBox2;
-        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox PrecioTxt;
+        private System.Windows.Forms.TextBox EmpaqueTxt;
+        private System.Windows.Forms.TextBox DescripcionTxt;
+        private System.Windows.Forms.TextBox NombreTxt;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Label label6;
         private System.Windows.Forms.Label label5;
@@ -211,7 +239,7 @@ namespace Tava.Forms
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.NumericUpDown UnidadesTxt;
         private System.Windows.Forms.PictureBox pictureBox1;
     }
 }
--- Tava/Tava/Forms/FormProductsRegistration.cs
+++ Tava/Tava/Forms/FormProductsRegistration.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Tava.Models;
 
 namespace Tava.Forms
 {
@@ -20,6 +21,69 @@ namespace Tava.Forms
         private void FormProductsRegistration_Load(object sender, EventArgs e)
         {
 
+        }
+
+
+        private void PrecioTxt_Click(object sender, EventArgs e)
+        {
+            if (PrecioTxt.Text == "0.00")
+            {
+                PrecioTxt.Text = "";
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (NombreTxt.Text == "" || UnidadesTxt.Value == 0 || PrecioTxt.Text == "" || PrecioTxt.Text == "0.00" || DescripcionTxt.Text == "" || EmpaqueTxt.Text == "Caja, bolsa, otros" || EmpaqueTxt.Text == "")
+            {
+                MessageBox.Show("Ingresa todos los valores requeridos para poder registrar un producto.");
+
+            }
+            else
+            {
+                try {
+                    using (var db = new TavaContext())
+                    {
+                        var product = new Product()
+                        {
+                            Name = NombreTxt.Text,
+                            Description = DescripcionTxt.Text,
+                            Unitsperset = Convert.ToInt32(Math.Round(UnidadesTxt.Value, 0)),
+                            Price = Convert.ToDouble(PrecioTxt.Text),
+                            Package = EmpaqueTxt.Text
+                        };
+
+                        db.Products.Add(product);
+                        db.SaveChanges();
+                        MessageBox.Show("El producto ha sido añadido con éxito.");
+                        
+                    }
+                }
+                catch
+                {
+                    MessageBox.Show("Datos invalidos, intente de nuevo.");
+                    
+                }
+                Limpiar();
+            }
+        
+        }
+
+        public void Limpiar()
+        {
+            NombreTxt.Clear();
+            DescripcionTxt.Clear();
+            UnidadesTxt.Value = 1;
+            PrecioTxt.Text = "0.00";
+            EmpaqueTxt.Text = "Caja, bolsa, otros";
+        }
+
+        private void EmpaqueTxt_Click(object sender, EventArgs e)
+        {
+            if (EmpaqueTxt.Text == "Caja, bolsa, otros")
+            {
+                EmpaqueTxt.Text = "";
+            }
         }
     }
 }
