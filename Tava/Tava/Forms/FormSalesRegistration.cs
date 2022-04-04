5de4c33e6e0368ad4d349e83e062e9684dee89f6
Andrea Velásquez <67344634+andreavvel@users.noreply.github.com>
2022-04-03 22:51:06 -0600

    delete instructions 🍙

--- Tava/Tava/Forms/FormSalesRegistration.cs
+++ Tava/Tava/Forms/FormSalesRegistration.cs
@@ -16,5 +16,44 @@ namespace Tava.Forms
         {
             InitializeComponent();
         }
+
+        //Clear instruction text from textbox
+        private void textBox1_Enter(object sender, EventArgs e)
+        {
+            textBox1.Text = "";
+        }
+
+        private void textBox2_Enter(object sender, EventArgs e)
+        {
+            textBox2.Text = "";
+        }
+
+        private void textBox3_Enter(object sender, EventArgs e)
+        {
+            textBox3.Text = "";
+        }
+
+        private void textBox6_Enter(object sender, EventArgs e)
+        {
+            textBox6.Text = "";
+        }
+
+        private void textBox5_Enter(object sender, EventArgs e)
+        {
+            textBox5.Text = "";
+        }
+
+        //When registration button is pressed
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string name = textBox1.Text;
+            string lastname = textBox2.Text;
+            string phone = textBox3.Text;
+
+            using (var db = new Tava.Models.TavaContext())
+            {
+
+            }
+        }
     }
 }
