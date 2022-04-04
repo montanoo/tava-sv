aa385541103407283eea9f0fab74d9f7a3654328
Luciana <lucianamunguia@gmail.com>
2022-04-03 23:03:44 -0600

    Added Products screen😈

--- Tava/Tava/Forms/FormProducts.cs
+++ Tava/Tava/Forms/FormProducts.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Tava.Models;
 
 namespace Tava.Forms
 {
@@ -15,6 +16,29 @@ namespace Tava.Forms
         public FormProducts()
         {
             InitializeComponent();
+            using (var db = new TavaContext())
+            {
+                var list = db.Products;
+                foreach (var prod in list)
+                {
+                    dataGridView1.Rows.Add(prod.Id, prod.Name, prod.Description);
+                }
+            }
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            
+           
+            
+        }
+
+        private void textBox1_Enter(object sender, EventArgs e)
+        {
+            if (textBox1.Text == "Buscar...")
+            {
+                textBox1.Text = "";
+            }
         }
     }
 }
