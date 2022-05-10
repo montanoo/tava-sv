1d6e0a8fee8d8bd535a1b221b3739a41c98a9997
Ivania <ivanialebron4.180@gmail,com>
2022-05-09 21:09:03 -0600

    Fill Sales and Client Tables 👩‍👩‍👧

--- Tava/Tava/Forms/FormCustomers.cs
+++ Tava/Tava/Forms/FormCustomers.cs
@@ -7,6 +7,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Tava.Models;
+
 
 namespace Tava.Forms
 {
@@ -15,6 +17,7 @@ namespace Tava.Forms
         //variables for search bar
         public DataTable input;
         string filterField = "Nombre";
+        int counterid = 1;
         public FormCustomers()
         {
             InitializeComponent();
@@ -25,9 +28,15 @@ namespace Tava.Forms
             using (var db = new Models.TavaContext())
             {
                 var list = db.Clients;
+                Billing billings = new Billing();
+                var list1 = db.Billings;
                 foreach (var cli in list)
                 {
-                    dataGridView1.Rows.Add(cli.Id, cli.Name + " " + cli.Lastname, cli.Phone, "no disponible");
+
+                    var quantity =  db.Billings.GroupBy(a => a.ClientId).Select(g => g.Count()).FirstOrDefault();
+                    
+                    
+                    dataGridView1.Rows.Add(cli.Id, cli.Name + " " + cli.Lastname, cli.Phone, quantity);
                 }
             }
         }
--- Tava/Tava/Forms/FormSales.cs
+++ Tava/Tava/Forms/FormSales.cs
@@ -7,14 +7,33 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Tava.Models;
 
 namespace Tava.Forms
 {
     public partial class FormSales : Form
     {
+        DialogResult dr;
         public FormSales()
         {
             InitializeComponent();
+            Sales();
+        }
+        private void Sales()
+        {
+            using (var db = new TavaContext())
+            {
+                var list = db.Billings;
+                Client clients = new Client();
+                foreach (var bill in list)
+                {
+                    Client getclid = db.Clients.Where(a => a.Id == bill.ClientId).FirstOrDefault();
+                    var names = getclid.Name +" "+ getclid.Name;
+                    Product getproductid = db.Products.Where(a => a.Id == bill.ProductId).FirstOrDefault();
+                    Pointofsale gettypeid = db.Pointofsales.Where(a => a.Id == bill.PointofsaleId).FirstOrDefault();
+                    dataGridView1.Rows.Add(bill.Id, bill.Date, names,getproductid.Name,gettypeid.Type,bill.Quantity, bill.Totalprice);
+                }
+            }
         }
     }
 
